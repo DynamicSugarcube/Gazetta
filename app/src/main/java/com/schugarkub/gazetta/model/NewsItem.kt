8a64a0314baf9a3c98b12b276f10306dbedec9fe
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2020-07-09 18:30:30 +0300

    (ADD) - Create NewsItem entity

---
+++ app/src/main/java/com/schugarkub/gazetta/model/NewsItem.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020. Schugarkub
+ */
+
+package com.schugarkub.gazetta.model
+
+import java.util.Date
+
+data class NewsItem(
+    val title: String,
+    val author: String,
+    val date: Date,
+    val imageUrl: String,
+    val url: String
+)
